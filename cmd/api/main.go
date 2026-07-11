836084167b621b5dbc982501b5936c948699da9a
Eddie <104090601+PixllCreations@users.noreply.github.com>
2026-07-10 23:28:34 -0700

    feat(cmd): wire Postgres store into API entrypoint

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,13 +1,34 @@
 package main
 
 import (
+	"context"
 	"log"
+	"os"
 
 	"github.com/pixll/atlas/api"
+	"github.com/pixll/atlas/store"
 )
 
 func main() {
-	if err := api.New(":8080").Run(); err != nil {
+	ctx := context.Background()
+
+	dsn := os.Getenv("ATLAS_DATABASE_URL")
+	if dsn == "" {
+		dsn = "postgres://atlas:atlas@localhost:5432/atlas?sslmode=disable"
+	}
+
+	st, err := store.New(ctx, dsn)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer st.Close()
+
+	addr := os.Getenv("ATLAS_ADDR")
+	if addr == "" {
+		addr = ":8080"
+	}
+
+	if err := api.New(addr, st).Run(); err != nil {
 		log.Fatal(err)
 	}
 }
